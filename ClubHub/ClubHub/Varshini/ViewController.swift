820c270e940a55d25d2cc2ab5da630da3268c60c
Srivarshini Ananta <sananta@ucdavis.edu>
2018-11-26 10:42:30 -0800

    Scrollable Calendar View

---
+++ ClubHub/ClubHub/Varshini/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SimpleCal
+//
+//  Created by Srivarshini Ananta on 11/24/18.
+//  Copyright © 2018 Srivarshini Ananta. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var eventBar: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let calendar = FSCalendar(frame: CGRect(x: 10, y: 30, width: 400, height: 300))
+        calendar.dataSource = self
+        calendar.delegate = self
+        view.addSubview(calendar)
+        self.calendar = calendar
+    }
+    
+    // Display the events for that particular date on the event board below the calendar
+    func calendar(calendar: FSCalendar!, didSelectDate date: NSDate!) {
+        
+    }
+    
+}
+
