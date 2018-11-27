9a1a5effc9cdb74ffa1e0a1d278fad6647f5dcea
Cindy <cindynhoang@gmail.com>
2018-11-26 23:38:17 -0800

    Display event details

--- ClubHub/ClubHub/EventDetailsViewController.swift
+++ ClubHub/ClubHub/EventDetailsViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class EventDetailsViewController: UIViewController {
 
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var clubNameLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     var event: Event?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EE MMM dd, yyyy hh:mm a"
+
         event?.printEvent()
+        eventNameLabel.text = event?.name ?? "Event Name"
+        clubNameLabel.text = event?.club ?? "Club Name"
+        dateTimeLabel.text = event?.startTime.map{dateFormatter.string(from: $0)} ?? "Date and Time"
+        locationLabel.text = event?.location ?? "Location"
+        descriptionLabel.text = event?.details ??  "Description"
     }
     
 
