da698c63ed7cace131de64b75c27ff3b5fdcc735
Srivarshini Ananta <sananta@ucdavis.edu>
2018-12-03 17:04:48 -0800

    Added user class for editing user settings while user is using ClubHub app

---
+++ ClubHub/ClubHub/User.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ClubHub
+//
+//  Created by Srivarshini Ananta on 12/3/18.
+//  Copyright © 2018 Lindsey Gray. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class User {
+    var id: String?
+    var club: Club?
+    var events: [String]?
+    var clubs: [String]?
+}
