bce3bfa11049e4aaba937e0de91d21700bb6e33e
Srivarshini Ananta <sananta@ucdavis.edu>
2018-12-03 17:04:48 -0800

    Added user class for editing user settings while user is using ClubHub app Added init functions for user class

---
+++ ClubHub/ClubHub/User.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  ClubHub
+//
+//  Created by Srivarshini Ananta on 12/3/18.
+//  Copyright © 2018 Lindsey Gray. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class User {
+    var id: String?
+    var club: Club?
+    var events: [String]?
+    var clubs: [String]?
+    
+    // Default
+    init() {
+        self.id = nil
+        self.club = nil
+        self.events = nil
+        self.clubs = nil
+        //self.images = nil
+    }
+    
+    init(id: String?, club: Club?, events: [String]?, clubs: [String]?) {
+        self.id = id
+        self.club = club
+        self.events = events
+        self.clubs = clubs
+        //self.images = nil
+    }
+}
