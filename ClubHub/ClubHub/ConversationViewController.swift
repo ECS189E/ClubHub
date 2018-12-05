7edc31dc43328435f43674523653fff141bf09c2
Cindy <cindynhoang@gmail.com>
2018-12-05 00:23:28 -0800

    Setup View Controller for conversations

---
+++ ClubHub/ClubHub/ConversationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ConversationViewController.swift
+//  ClubHub
+//
+//  Created by Cindy Hoang on 12/5/18.
+//  Copyright © 2018 Lindsey Gray. All rights reserved.
+//
+
+import Firebase
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
